f9ce4d1d5bc039673d229340fca51aef881296db
uepricky <10969falilv@gmail.com>
2022-06-24 18:31:58 +0900

    1:15:28まで完了

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 
 const App = () => {
-    const activeMenu = false;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
